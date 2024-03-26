dc3a6e9649af836a5f903a2dc4209f9c2de70d3d
Zero-7-1 <109064402+Zero-7-1@users.noreply.github.com>
2024-03-26 10:53:53 +0530

    worked on outputs

--- 01_output.php
+++ 01_output.php
@@ -1,6 +1,6 @@
 <?php 
 /*This is a php tag. If there is no html or other content below the php, 
-we don't need to close the php tag which can be found at the last. 
+  we don't need to close the php tag which can be found at the last. 
 
 -> A PHP script can be placed anywhere in the document.
 -> A PHP script is executed on the server, and the plain HTML result 
@@ -18,19 +18,30 @@ is sent back to the browser.
 
 
 // Echo - Output strings, numbers, even html
-echo 'Hello PHP' .PHP_EOL;
-echo 996969   .PHP_EOL;
-echo '<h1>Hello-PHP </h1>';
-            // .PHP_EOL similar to new line character (works best for multi os than /t)
+echo "Hello PHP \n";
+echo 10101010;
+echo '<h1> Hello PHP </h1>';
+            /* To output content in PHP on a new line, 
+            we can use the newline character \n  inisde double quotes
+            or <br> tag for html contents  */
+            // .PHP_EOL similar to new line character (works good for multi os)
 
 
-// we can also use print but it take only single argument 
-      // and () is purely optional and better to avoid it 
-print 'printPHP' .PHP_EOL;
+// we can also use print in place of echo but it takes only single argument 
+      // and () is purely optional both for echo and print and better to avoid it 
+print 'printPHP' . "\n";
+           /* The \n inside the double quotes (" ") is interpreted correctly 
+              as a newline character, but the single quotes (' ') around 'printPHP'
+              prevent PHP from interpreting \n as a newline character. 
+              Instead, it treats it as part of the string.
+              To fix this, we can either concatenate the newline character
+              yes in PHP we use dot for concat or 
+              use double quotes around the entire string */
 
-// print_r - Gives a bit more info. Can be used to print arrays
-    // it is a function so here we need () 
-print_r([1, 2, 3])  .PHP_EOL;
+// print_r - Gives a bit more info, used to print arrays
+ // it is a function so here we need () 
+print_r([1, 2, 3])  . "\n";
+  // we will do explanation of the output in arrays file 
 
 // var_dump - Provides more info like data type and length
 var_dump('Hello') .PHP_EOL;
@@ -40,10 +51,7 @@ var_dump([1, 2, 3]) .PHP_EOL;
 // these outputs will look ugly for sure but intersting things will come .. 
 ?>
   
-
-
-
-<!-- You can output any HTML that you want within a .php file -->
+<!-- We can output any HTML that we want within a .php file -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -54,6 +62,6 @@ var_dump([1, 2, 3]) .PHP_EOL;
 </head>
 <body>
   <!-- We can output PHP including variables, etc -->
-  <h1>Hello <?php echo 'PHP'; ?></h1>
+  <h3>Hello <?php echo 'from PHP'; ?></h3>
 </body>
 </html>
