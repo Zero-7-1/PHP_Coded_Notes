24bb54261cb5670d1959251fb68c85df0ef14d5c
Zero-7-1 <109064402+Zero-7-1@users.noreply.github.com>
2024-03-26 11:48:26 +0530

    working on arrays

--- 03_arrays.php
+++ 03_arrays.php
@@ -1,6 +1,19 @@
 <?php
-/* Arrrays are data types or data structures that holds multiple values  */
+/* Arrrays are special variables that holds multiple values 
+   In PHP, there are three types of arrays:
+    Indexed arrays - Arrays with a numeric index
+    Associative arrays - Arrays with named keys
+    Multidimensional arrays - Arrays containing one or more arrays
+ */
 
+
+
+
+
+ /* In indexed arrays each item has an index number.
+   By default, the first item has index 0, the second item has item 1, etc. 
+   To access an array item you can refer to the index number
+   To change the value of an array item, use the index number  */
 
 // Simple array of numbers using sq. brace 
 $numbers = [1, 2, 3, 4, 5];
@@ -11,16 +24,24 @@ $fruits = array('apple', 'banana');
 print_r($fruits);
 var_dump($fruits);
 
+
 // Outputting values from an array
-echo $numbers[0];
+echo $numbers[0] . "\n";
 echo $numbers[3] + $numbers[4];
 
+
+
 // We can use print_r or var_dump to see the contents of an array
-var_dump($numbers);
+// var_dump($numbers);
+
+
+
+
+
 
 /*
   Associative arrays allow us to use named keys to identify values.
-*/
+
 
 $colors = [
   1 => 'red',
@@ -43,7 +64,7 @@ var_dump($hex);
 /*
   Multi-dimansional arrays are often used to store data in a table format. Arrays within
    arrays 
-*/
+
 
 // Single person
 $person1 = [
@@ -72,6 +93,6 @@ var_dump($people);
 // Accessing values in a multi-dimensional array
 echo $people[0]['first_name'];
 echo $people[2]['email'];
-
+*/
 
 
