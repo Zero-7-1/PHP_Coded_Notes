fdf60d857acb3a9d7f0e4c0df5a2cfce38ea1b95
Zero-7-1 <109064402+Zero-7-1@users.noreply.github.com>
2024-05-01 16:38:24 +0530

    changes in 2

--- 2 - VarDataTypes.php
+++ 2 - VarDataTypes.php
@@ -3,23 +3,25 @@
 
 /*
  PHP Data Types
-- String - A string is a series of characters surrounded by quotes
-- Integer - Whole numbers
-- Float - Decimal numbers
-- Boolean - true or false
-- Array - An array is a special variable, which can hold more than one value
-- Object - A class
-- NULL - Empty variable
-- Resource - A special variable that holds a resource
+1. String - A string is a series of characters surrounded by quotes
+2. Integer - Whole numbers
+3. Float - Decimal numbers
+4. Boolean - true or false
+5. Array - An array is a special variable, which can hold more than one value
+6. Object - Instance of a Class
+7. NULL - Empty variable
+8. Resource - A special variable that holds a resource
 
+We can get the data type of any object by using the var_dump()
 PHP has no command for declaring a variable. 
 It is created the moment we first assign a value to it.
+But, Variables must be prefixed with $
+
+PHP has some predefined variables (12 predefined variables in php 8) like
+$GLOBALS, Superglobals, $_GET, $_POST etc.. which we shall see  
 */
 
-/*
-  PHP Variable Rule 
-- Variables must be prefixed with $
-*/
+
 
 $name = 'Sudhanshu'; // String, Can be in single or double quotes
 $age = 25; // Integer
@@ -34,7 +36,7 @@ echo "$name is $age years old \n";
 
 
 
-// more about Strings .. 
+   // more about Strings .. 
 
 // strlen() function returns the length of a string
 echo strlen("Sudhanshu") . "\n";   // outputs 9
@@ -70,6 +72,38 @@ echo substr($greet, 0, 5) . "\n"; // Specify the start index and end index
   // The last character has index -1
  
 
+// NULL value - no value assigned to it
+  // If a variable is created without a value,
+     //  it is automatically assigned a value of NULL.
+
+
+/* PHP Resource - storing reference to functions and resources external 
+to PHP. A common example of using the resource data type is a database call 
+(Details about Resource later) */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     /* Casting allows us to change the data type of an existing variable, 
+   but not by changing the actual value */
+
+
+
 
 
 
@@ -90,22 +124,16 @@ the strict requirement, it will throw a "Fatal Error" on a type mismatch.
 */
 
 
-// Constants - Cannot be changed, like db credentials 
-define('HOST', 'localhost');
-define('DB_name', 'dev_db');
-echo(HOST);
-
-
-
-// PHP Variables Scope 
-/* The scope of a variable is the part of the script where the variable can be 
-referenced or used. 
-PHP has two different variable scopes:
+// Variables Scope 
+/* The scope of a variable is the part where the variable can be referenced or used. 
+PHP has 3 different variable scopes:
 local -  declared within a function has a LOCAL SCOPE and 
          can only be accessed within that function
     
 global - declared outside a function has a GLOBAL SCOPE and 
         can only be accessed outside a function and not inside a function 
+
+static - does not lose its value when program execution leaves the scope
 */
 
 
@@ -113,29 +141,43 @@ global - declared outside a function has a GLOBAL SCOPE and
 // global, we already outputed global scoped varibles, those can't be acessed inside 
      // a function , we will see about function in function file 
 
-// global keyword is used to access a global variable from within a function
+// 'global' keyword is used to access a global variable from within a function
 
      function testFunction() {
       $x = 5; // local scope
-      global $name; // using global keyword 
+      global $name; // used 'global' keyword and this $name is defined outside   
       echo "<h3> Variable inside function is: $x </h3>";
      //  echo "<h3>Variable inside function is: $name</h3>"; UNDEFINED 
-     echo "<h3> Using global keyword with global Variable inside function, we can 
+     echo "<h3> Using 'global' keyword with global Variable inside function, we can 
                 access the global varibale that is $name </h3>";
 
      }
      testFunction();
-     echo "<h3> Using global Variable inside function that is: $name will show 
-                undefined </h3>";
+
+  /* PHP also stores all global variables in an array called $GLOBALS[index]. 
+    The index holds the name of the variable. This array is also accessible from 
+    within functions and can be used to update global variables directly. */
+
+  // so alternative to 'global' keyword we can do like this 
+  
+   $number1 = 10;
+   $number2 = 15;
+
+   function mySum() {
+   $GLOBALS['number2'] = $GLOBALS['number1'] + $GLOBALS['number2'];
+   }
+   mySum();
+   echo $number2 . "\n";  // 25 
+
 
 // We can have local variables with the same name in different functions, 
   // because local variables are only recognized by the function
         // in which they are declared.
 
-/* static keyword - when a function is completed/executed, all of its variables are 
-                    deleted. However, sometimes we want a local variable NOT to 
-                    be deleted. We need it for a further job. To do this, 
-                    use the static keyword when first declare the variable */
+/* 'static' keyword - when a function is completed/executed, all of its variables are 
+                     deleted. However, sometimes we want a local variable NOT to 
+                     be deleted. We need it for a further job. To do this, 
+                     use the static keyword when first declare the variable */
 function testFun() {
     static $staticVAR = 1 . "\n";
     echo $staticVAR;
@@ -144,3 +186,29 @@ function testFun() {
 
 testFun(); // 1
 testFun(); // 2 and so on .. 
+
+
+/* variable lifecycle:
+  Before a variable is used, it has no existence. It is unset. 
+  It is possible to check if a variable doesn't exist by using isset().
+  This returns true provided the variable exists and isn't set to null. 
+  With the exception of null, the value a variable holds plays no part in 
+  determining whether a variable is set. Setting an existing variable to null 
+  is a way of unsetting a variable. Another way is variables may be destroyed by 
+  using the unset().*/
+
+  var_dump(isset($a)); // returns false as $a was not set 
+  
+  $bas = null;
+  var_dump(isset($bas)); // returns also false 
+  
+// A variable can be tested for emptiness using empty();
+
+// PHP Variable variables
+/* In PHP, it is possible to set variable name dynamically. 
+  Such a variable uses value of an existing variable as name. 
+  A variable variable is defined with two $ signs as prefix */
+
+  $a = "Hello";
+  $$a = "PHP"; // This creates a variable named $Hello with the value "PHP" 
+  echo $$a;
