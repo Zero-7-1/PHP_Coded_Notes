2634b7c4a46e83b867cf0ffbd9bbb45fa98eae07
Zero-7-1 <109064402+Zero-7-1@users.noreply.github.com>
2024-07-21 11:55:32 +0530

    Fundamentals Folder Separated

---
+++ 5-Function.php
@@ -0,0 +1,43 @@
+<?php
+// User Defined Function 
+/*
+A user-defined function declaration starts with the keyword function, 
+followed by the name of the function. Information can be passed to functions through 
+arguments and parameters. 
+Parameters are the variables that are defined in the 
+function signature (or declaration). They act as placeholders for the values that
+will be passed to the function when it is called. 
+Arguments are the actual values that
+are passed to the function when it is called. They are the real data that the function
+processes. 
+Also we have Default Argument Value - If we call the function without arguments it 
+takes the default parameter value as argument. 
+Additionally, functions can either return a value or perform an action without 
+returning a value. With return functions compute and return a value. 
+*/
+
+function studentData($name, $class) {
+    echo $name . " of " . $class;
+}
+studentData("John Doe", "Class 10");
+
+echo "<br>";
+echo "<br>";
+
+function setHeight($minheight = 50) {
+    echo "The height is : $minheight";
+  }
+ setHeight(350);
+ echo "<br>";
+ setHeight(); // will use the default value of 50
+
+ echo "<br>";
+ echo "<br>";
+
+ function sumTwo($a, $b) {
+    return $a + $b;
+ }
+echo sumTwo(5,5);
+
+
+?>
